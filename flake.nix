9636f26f1a763fbbaf581debea7f0d9221b61c9d
Tom McLaughlin <tom@codedown.io>
2026-02-01 19:03:28 -0800

    Tidy flake.nix

--- flake.nix
+++ flake.nix
@@ -1,29 +1,18 @@
 {
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.gitignore = {
-    url = "github:hercules-ci/gitignore.nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
-  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-25.11";
 
-  outputs = { self, flake-utils, gitignore, haskellNix, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            haskellNix.overlay
-            # (import ./nix/fix-ghc-pkgs-overlay.nix)
-          ];
-          inherit (haskellNix) config;
+          # overlays = [
+          #   haskellNix.overlay
+          #   # (import ./nix/fix-ghc-pkgs-overlay.nix)
+          # ];
+          # inherit (haskellNix) config;
         };
-
-        src = gitignore.lib.gitignoreSource ./.;
-
-        compilerNixName = "ghc9122";
-
-
       in
         {
           devShells = {
